9e924dddd5f6aaa703f5473fb5c2a87d4270a1ae
Trent Cornwell <tccornwell@mail.bradley.edu>
2016-03-17 08:46:26 -0500

    health bar stuff
    i hope this works

---
+++ SuperHyperPunch/Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour {
+	
+	public float max_Health = 100f;
+	public float cur_Health = 0f;
+	public float healAmount = 10f;
+	public GameObject healthBar;
+	public Text healthText;
+	
+	// Use this for initialization
+	void Start () {
+		cur_Health = max_Health;
+		//just for testing, need to replace this with a call to the damage system.
+		//InvokeRepeating ("DecreaseHealth", 1f, 5f);
+		//InvokeRepeating ("IncreaseHealth", 2f, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthText.text = "Health: " + cur_Health;
+	}
+	
+	void IncreaseHealth(){
+		
+		if(cur_Health < 100f)
+		{
+			cur_Health += healAmount;
+			float calc_Health = cur_Health / max_Health;
+			SetHealthBar (calc_Health);
+			
+			if(cur_Health > max_Health){
+				cur_Health = max_Health;
+			}
+		}
+	}
+	
+	void DecreaseHealth(){
+		//just for testing, need to replace this with a damage system.
+		cur_Health -= 50f;
+		float calc_Health = cur_Health / max_Health;
+		SetHealthBar (calc_Health);
+	}
+	
+	public void SetHealthBar(float myHealth){
+		healthBar.transform.localScale = new Vector3(Mathf.Clamp(myHealth,0f ,1f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+	
+}
---
+++ SuperHyperPunch/Assets/Scripts/MovementandHealthScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MovementandHealthScript : MonoBehaviour {
+	
+	public Slider healthBarSlider;  //reference for slider
+	public Text gameOverText;   //reference for text
+	private bool isGameOver = false; //flag to see if game is over
+	
+	//Energy Variables
+	public Slider energyBarSlider;  //reference for slider
+	//public float max_Energy = 1.0f;
+	//public float cur_Energy = 0f;
+	public float powerAmount = 0.3f;
+	
+	void Start(){
+		gameOverText.enabled = false; //disable GameOver text on start
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check if game is over i.e., health is greater than 0
+		if(!isGameOver)
+			transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*10f, 0, 0); //get input
+			
+			IncreaseEnergy();
+			DecreaseEnergy();
+	}
+	
+	//Check if player enters/stays on the fire
+	void OnTriggerStay(Collider other){
+		//if player triggers fire object and health is greater than 0
+		if(other.gameObject.name=="OtherPlayer" && healthBarSlider.value>0){
+			healthBarSlider.value -=1.0f;  //reduce health
+		}
+		else{
+			isGameOver = true;    //set game over to true
+			gameOverText.enabled = true; //enable GameOver text
+		}
+	}
+	
+	void IncreaseEnergy(){
+		/*
+		if(cur_Energy < 1.0f)
+		{
+			cur_Energy += powerAmount;
+			float calc_Health = cur_Energy / max_Energy;
+			
+			if(cur_Energy > max_Energy){
+				cur_Energy = max_Energy;
+			}
+		}*/
+		
+		
+		if(Input.GetKeyDown("i"))
+		{
+			energyBarSlider.value += powerAmount;
+		}
+	}
+	
+	void DecreaseEnergy(){
+		//just for testing, need to replace this with a energy system.
+		if(Input.GetKeyDown("o"))
+		{
+			energyBarSlider.value -= powerAmount;
+		}
+	}
+}
